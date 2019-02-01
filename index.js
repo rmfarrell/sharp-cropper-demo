af1f371e1f7f5588db70b06a5d796cba9190f050
Farrell <rfarrell@RFarrell-MBP.local>
2019-02-01 11:11:51 -0500

    add comments

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 const sharp = require('sharp'),
   fs = require('fs'),
-  Cropper = require('./cropper')
+  Cropper = require('./cropper'),
+  srcImagePath = './test.jpg',
+  outputImgPath = './out.jpg',
 
-// main()
+  // focus of image
+  focus = {
+    x: 100,
+    y: 300
+  },
 
-getCropBox()
+  // output dimensions
+  target = [
+    200, // width
+    300  // height
+  ]
 
-const target = [
-  200, // width
-  300  // height
-]
+main()
 
-function getCropBox() {
-  const image = sharp(fs.readFileSync('./test.jpg'))
+const
+
+function main() {
+  const image = sharp(fs.readFileSync(srcImagePath))
 
   image
+    // get the height and width of the original image
     .metadata()
     .then(({ height, width }) => {
-      let top = 0,
-        left = 0
-      const cropper = new Cropper({ height, width }, { x: 100, y: 300 })
+      const cropper = new Cropper({ height, width }, focus)
       const cropped = cropper
         .crop(...target)
         .map((val) => Math.floor(val))
 
+      // cropped outputs an image that is large enough to fill the the frame (resizedWidth/resizedHeight)
+      // it's ceentered itn place and then shifted to fill the frame
+      // x and y are offset of the resized image used to fill the space of the output image
+      // since the resized image is guaranteed to leave no gaps x and y will always be either 0 or negative.
+      const [
+        x, // x-position of resized image
+        y, // y-position of same
+        resizedWidth, // width of resized image
+        resizedHeight // height of resized image
+      ] = croppped
+
       console.log('\n-- cropped\n')
       console.log(cropped)
 
-      left = Math.abs(cropped[0])
-
-      restHeight = cropped[2] - target[1]
-
-      console.log('resth', cropped[2] - target[0])
-
-      top = restHeight + cropped[1]
-
+      // this is the param that extract taks
       const area = {
         left: cropped[2] - target[0] + cropped[0],
         top: cropped[3] - target[1] + cropped[1],
@@ -42,13 +54,13 @@ function getCropBox() {
         height: target[1]
       }
 
-      const diff = cropped[3] - target[1]
-
       console.log('\n-- area\n')
       console.log(area)
 
       return image
+        // resizing the image to the resized image (larger than the actual output)
         .resize(...cropped.slice(2))
+        // extract the pixels from the resized image which corresponds to the target height and width
         .extract(area)
         .toFile('out.jpg', (err, info) => {
           if (err) {
@@ -56,17 +68,5 @@ function getCropBox() {
           }
           console.log(info)
         })
-    })
-}
-
-function main() {
-
-  sharp(fs.readFileSync('./test.jpg'))
-    .resize(200, 200)
-    .toFile('./test2.jpg', (err, info) => {
-      if (err) {
-        throw err
-      }
-      console.log(info)
     })
 }
